65c47fed76cae62d44ae82237689c76d9688641c
samuelGoupil <samuelgoupil@etu.unilim.fr>
2023-01-06 10:04:53 +0100

    Exo 1 terminé

--- Exo1/exo1.py
+++ Exo1/exo1.py
@@ -8,24 +8,80 @@ class Robot():
     __battery_level = 0
     __states = ['shutown', 'running']
     
-    def __init__(self, nom):
-        self.__name = nom
+    def __init__(self, name, power, battery, speed):
+        self.__name = name
+        self.__power = power
+        self.__battery_level = battery
+        self.__current_speed = speed
+        
 
     def allumage(self):
-        __power = True
-        self.print_state()
+        print("switch on")
+        self.__power = True
+        
+       
 
     def eteindre(self):
-        __power = False
-        self.print_state()
+        print("switch off")
+        self.__power = False
+        
+       
 
     def recharge(self):
-        while __battery_level <100:
-            time.sleep(2)
-            __battery_level = __battery_level + 10
+        while self.__battery_level < 100:
+            time.sleep(0.1)
+            self.__battery_level = self.__battery_level + 1
+            print(self.get_battery())
+
+    def get_name(self):
+        print("Le robot s'appelle " + self.__name) 
+
+    def get_power(self):
+        if self.__power == False :
+            print('OFF')
+        else : 
+            print('ON')
+
+    def get_battery(self):
+        return self.__battery_level
+
+    def set_speed(self, speed):
+        self.__current_speed = speed
+    
+    def get_speed(self):
+        print('La vitesse du robot est de '+f'{self.__current_speed}' +'km/h')
+
+    def stop(self):
+        self.__current_speed = 0
+        print('le robot est arreté')
+
+    def get_states(self):
+        if self.__power == False:
+            print(self.__states[0])
+        else :
+            print(self.__states[1])
+
+walle = Robot('Walle',False,0,0)
+
+print(walle.get_name())
+print(walle.get_power())
+walle.allumage()
+print(walle.get_power())
+print(walle.get_speed())
+walle.set_speed(15)
+print(walle.get_speed())
+walle.stop()
+print(walle.get_speed())
+print(walle.get_states())
+walle.eteindre()
+print(walle.get_states())
 
 
-print('salut')
-walle = Robot('Walle')
 
-print(walle.name)
+
+
+
+""" print(walle.get_battery())
+walle.recharge()
+print(walle.get_battery()) """
+
